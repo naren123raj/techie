7242d684cc0232322b0a2eccacc299db523929e5
venkata <venkata@kurapati>
2020-11-02 14:32:15 +0800

    new file

---
+++ src/main/java/com/my/techie/TechieApp.java
@@ -0,0 +1,13 @@
+package com.my.techie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechieApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TechieApp.class, args);
+	}
+
+}
